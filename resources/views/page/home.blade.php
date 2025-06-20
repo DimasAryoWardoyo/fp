1107322228b1968a0a8c0a82802ba41c22c47e19
Dimas Aryo Wardoyo <dimaswardoyo@students.amikom.ac.id>
2025-06-20 23:09:41 +0700

    feat: Add chefs section with dynamic content display for latest activities and categories

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -60,7 +60,81 @@
             </div>
         </section>
 
-        <section class="store-new-products">
+        <section class="chefs section-bg mb-4">
+            <div class="container" data-aos="fade-up">
+                <div class="section-header">
+                    <h5>Konten Terbaru</h5>
+                </div>
+                <div class="row gy-4">
+                    @php $incrementKonten = 0; @endphp
+                    @forelse ($kontens as $konten)
+                        <div class="col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementKonten += 100 }}">
+                            <div class="chef-member h-100 d-flex flex-column w-100">
+                                <div class="member-img w-100" style="height: 200px; overflow: hidden; border-radius: 8px;">
+                                    <img src="{{ asset('storage/' . $konten->gambar1) }}" alt="{{ $konten->nama_konten }}"
+                                        style="width: 100%; height: 100%; object-fit: cover;">
+                                </div>
+                                <div class="member-info d-flex flex-column w-100 mt-3">
+                                    <h4 class="text-truncate">
+                                        {{ \Illuminate\Support\Str::limit($konten->nama_konten, 50) }}</h4>
+                                    <p class="text-muted">
+                                        {{ \Carbon\Carbon::parse($konten->tanggal_konten)->format('d F Y') }}</p>
+                                    <a href="{{ route('konten.show', $konten->id) }}" class="mt-auto">Baca
+                                        Selengkapnya...</a>
+                                </div>
+                            </div>
+                        </div>
+                    @empty
+                        <div class="col-12 text-center py-5" data-aos="fade-up" data-aos-delay="100">
+                            <p class="text-muted">Tidak ada konten yang tersedia.</p>
+                        </div>
+                    @endforelse
+                </div>
+            </div>
+        </section>
+        <section class="chefs section-bg mb-4">
+            <div class="container" data-aos="fade-up">
+                <div class="section-header">
+                    <p>Kegiatan Yang Akan <span>Dilaksanakan</span></p>
+                </div>
+                <div class="row gy-4">
+                    @php $incrementEvent = 0; @endphp
+                    @forelse ($events as $index => $event)
+                        <div class="col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementEvent += 100 }}">
+                            <div class="chef-member h-100 d-flex flex-column w-100">
+                                <div class="member-img w-100" style="height: 200px; overflow: hidden; border-radius: 8px;">
+                                    @if ($event->foto)
+                                        <img src="{{ asset('storage/' . $event->foto) }}" alt="{{ $event->nama_agenda }}"
+                                            style="width: 100%; height: 100%; object-fit: cover;">
+                                    @else
+                                        <div style="background-color: #eee; width: 100%; height: 100%;"></div>
+                                    @endif
+                                </div>
+                                <div class="member-info d-flex flex-column w-100 mt-3">
+                                    <h4 class="text-truncate">{{ \Illuminate\Support\Str::limit($event->nama_agenda, 50) }}
+                                    </h4>
+                                    <p class="text-muted">
+                                        {{ \Carbon\Carbon::parse($event->waktu_mulai)->format('d F Y') }}
+                                    </p>
+                                    <a href="{{ route('agenda.show', $event->id) }}" class="mt-auto">Lihat Detail...</a>
+                                </div>
+                            </div>
+                        </div>
+                    @empty
+                        <div class="col-12 text-center py-5" data-aos="fade-up" data-aos-delay="100">
+                            <p class="text-muted">Belum ada kegiatan yang akan datang.</p>
+                        </div>
+                    @endforelse
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
+
+
+{{-- <section class="store-new-products">
             <div class="container">
                 <div class="row">
                     <div class="col-12" data-aos="fade-up">
@@ -101,15 +175,12 @@
                     @endforelse
                 </div>
             </div>
-        </section>
+        </section> --}}
 
-
-        <section class="store-new-products">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12" data-aos="fade-up">
-                        <h5>Kegiatan Yang Akan Dilaksanakan</h5>
-                    </div>
+{{-- <section class="chefs section-bg mb-4">
+            <div class="container" data-aos="fade-up">
+                <div class="section-header">
+                    <h5>Kegiatan Yang Akan Dilaksanakan</h5>
                 </div>
                 <div class="row">
                     @php $incrementEvent = 0 @endphp
@@ -145,7 +216,4 @@
                     @endforelse
                 </div>
             </div>
-        </section>
-
-    </div>
-@endsection
+        </section> --}}
--- resources/views/page/kategori.blade.php
+++ resources/views/page/kategori.blade.php
@@ -15,8 +15,8 @@
                         <div class="col-6 col-md-3 col-lg-2" data-aos="fade-up" data-aos-delay="{{ $loop->iteration * 100 }}">
                             <a href="{{ route('kategori.show', $item->id) }}" class="component-categories d-block">
                                 <div class="categories-image">
-                                    <img src="{{ asset('storage/' . $item->gambar_kategori) }}" alt="{{ $item->nama_kategori }}"
-                                        class="w-100">
+                                    <img src="{{ asset('storage/' . $item->gambar_kategori) }}"
+                                        alt="{{ $item->nama_kategori }}" class="w-100">
                                 </div>
                                 <p class="categories-text">
                                     {{ $item->nama_kategori }}
@@ -28,30 +28,34 @@
             </div>
         </section>
 
-
-        <section class="store-new-products">
-            <div class="container">
-                <div class="row">
+        <section class="chefs section-bg mb-4">
+            <div class="container" data-aos="fade-up">
+                <div class="section-header">
+                    <h5>Konten Terbaru</h5>
+                </div>
+                <div class="row gy-4">
+                    @php $incrementKonten = 0; @endphp
                     @forelse ($kontens as $konten)
-                        <div class="col-6 col-md-4 col-lg-3 mb-4" data-aos="fade-up"
-                            data-aos-delay="{{ $loop->iteration * 100 }}">
-                            <a href="{{ route('konten.show', $konten->id) }}" class="component-products d-block">
-                                <div class="products-thumbnail">
-                                    <div class="products-image"
-                                        style="background-image: url('{{ asset('storage/' . $konten->gambar1) }}');">
-                                    </div>
+                        <div class="col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementKonten += 100 }}">
+                            <div class="chef-member h-100 d-flex flex-column w-100">
+                                <div class="member-img w-100" style="height: 200px; overflow: hidden; border-radius: 8px;">
+                                    <img src="{{ asset('storage/' . $konten->gambar1) }}" alt="{{ $konten->nama_konten }}"
+                                        style="width: 100%; height: 100%; object-fit: cover;">
                                 </div>
-                                <div class="products-text">
-                                    {{ Str::limit($konten->nama_konten, 50) }}
+                                <div class="member-info d-flex flex-column w-100 mt-3">
+                                    <h4 class="text-truncate">
+                                        {{ \Illuminate\Support\Str::limit($konten->nama_konten, 50) }}</h4>
+                                    <p class="text-muted">
+                                        {{ \Carbon\Carbon::parse($konten->tanggal_konten)->format('d F Y') }}</p>
+                                    <a href="{{ route('konten.show', $konten->id) }}" class="mt-auto">Baca
+                                        Selengkapnya...</a>
                                 </div>
-                                <div class="products-price">
-                                    {{ $konten->tanggal_konten }}
-                                </div>
-                            </a>
+                            </div>
                         </div>
                     @empty
-                        <div class="col-12 text-center py-5" data-aos="fade-up">
-                            <p>Tidak ada konten dalam kategori ini.</p>
+                        <div class="col-12 text-center py-5" data-aos="fade-up" data-aos-delay="100">
+                            <p class="text-muted">Tidak ada konten yang tersedia.</p>
                         </div>
                     @endforelse
                 </div>
